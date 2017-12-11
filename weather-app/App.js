57c5d73f49270499c422d1dd06ea1ec9de28ec44
kbo <vanadis0929@gmail.com>
2017-12-12 01:26:47 +0900

    스타일 정의시 주의사항

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>테스트 입니다~~~~</Text>
-        <Text>테스트 입니다~~~~</Text>
-        <Text>테스트 입니다~~~~</Text>
+        <Text style={styles.color}>테스트 입니다~~~~</Text>
+        <Text style={styles.color}>테스트 입니다~~~~</Text>
+        <Text style={styles.color}>테스트 입니다~~~~</Text>
       </View>
       
     );
   }
 }
 
+//글자관련 속성은 <text></text>에서만 먹는다 안그러면 에러남 ㅇㅅㅇ
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  color:{
+    color: 'white',
+    fontSize: 30
+  }
 });
+
